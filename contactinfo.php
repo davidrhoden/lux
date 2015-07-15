d5b897f86ecbdccd9d0df4c98e18cd2b6ba79a80
David Rhoden <david@davidrhoden.com>
2015-07-15 14:04:39 -0500

    add reviews

--- contactinfo.php
+++ contactinfo.php
@@ -1,6 +1,8 @@
+<p>
 Lux Salon Blends<br />
 3141 Ponce de Leon, New Orleans LA 70119<br />
 info@lux-salonblends.com<br />
 <span class="bullet">T:</span> 504-301-2953 <span class="bullet">&bull;</span> <span class="bullet">F:</span> 504-947-5217
+</p>
 <div id="facebook_like_button">
 <iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2FLuxSalonBlends&amp;layout=button_count&amp;show_faces=false&amp;width=225&amp;action=like&amp;font=lucida+grande&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:225px; height:21px;" allowTransparency="true"></iframe>
